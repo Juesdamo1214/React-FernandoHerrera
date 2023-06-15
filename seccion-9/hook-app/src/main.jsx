cf7c6f32e4ad104665ae113890625674146256fd
Juesdamo1214 <68707535+Juesdamo1214@users.noreply.github.com>
2023-06-15 15:27:00 -0500

    GIT

--- seccion-9/hook-app/src/main.jsx
+++ seccion-9/hook-app/src/main.jsx
@@ -3,7 +3,12 @@ import ReactDOM from 'react-dom/client'
 import { HooksApp } from './HooksApp'
 
 import './index.css'
-import { Layout } from './05-useLayoutEffect/Layout'
+import { TodoApp } from './08-useReducer/TodoApp'
+// import { Padre } from './07-tarea-memo/Padre'
+// import { CallbackHook } from './06-memos/CallbackHook'
+// import { MemoHook } from './06-memos/MemoHook'
+// import { Memorize } from './06-memos/Memorize'
+// import { Layout } from './05-useLayoutEffect/Layout'
 // import { FocusScreen } from './04-useRef/FocusScreen'
 // import { MultipleCustomHooks } from './03-example/MultipleCustomHooks'
 // import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook'
@@ -11,9 +16,10 @@ import { Layout } from './05-useLayoutEffect/Layout'
 // import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
 // import { CounterApp } from './01-useState/CounterApp'
 
+// import './08-useReducer/intro-reducer';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
-    <Layout />
-  // </React.StrictMode>
-)
+    <TodoApp />
+   // </React.StrictMode>
+ )
